0f379eb630bc4f2e4bfb16556d831cf3b328fc38
alon24 <htpcfreak@gmail.com>
2015-11-23 12:36:45 +0200

    multiple click event on long click

--- include/utils/ButtonActions.h
+++ include/utils/ButtonActions.h
@@ -23,7 +23,7 @@
  4) Long Press and Hold:  holding the button for a long time
  */
 
-enum ButtonAction { BTN_CLICK=1, BTN_DOUBLE_CLICK=2, BTN_LONG_CLICK=3, BTN_HOLD_CLICK=4};
+enum ButtonAction { BTN_CLICK=1, BTN_DOUBLE_CLICK=2, BTN_LONG_CLICK=3, BTN_HOLD_CLICK=4, BTN_HOLD_RUNEVENTS};
 
 struct ButtonUseEvent {
 	const ButtonAction &action;
@@ -41,85 +41,95 @@ class ButtonActions
 //  MULTI-CLICK:  One Button, Multiple Events
 
 // Button timing variables
-		int debounce = 20; // ms debounce period to prevent flickering when pressing or releasing the button
-		int DCgap = 250;       // max ms between clicks for a double click event
-		int holdTime = 1000; // ms hold period: how long to wait for press+hold event
-		int longHoldTime = 3000; // ms long hold period: how long to wait for press+hold event
+	int debounce = 20; // ms debounce period to prevent flickering when pressing or releasing the button
+	int DCgap = 250;       // max ms between clicks for a double click event
+	int holdTime = 400; // ms hold period: how long to wait for press+hold event
+	int longHoldTime = 3000; // ms long hold period: how long to wait for press+hold event
 
 // Button variables
-		boolean buttonVal = HIGH;   // value read from button
-		boolean buttonLast = HIGH; // buffered value of the button's previous state
-		boolean DCwaiting = false; // whether we're waiting for a double click (down)
-		boolean DConUp = false; // whether to register a double click on next release, or whether to wait and click
-		boolean singleOK = true;    // whether it's OK to do a single click
-		long downTime = -1;         // time the button was pressed down
-		long upTime = -1;           // time the button was released
-		boolean ignoreUp = false; // whether to ignore the button release because the click+hold was triggered
-		boolean waitForUp = false; // when held, whether to wait for the up event
-		boolean holdEventPast = false; // whether or not the hold event happened already
-		boolean longHoldEventPast = false; // whether or not the long hold event happened already
-		int m_buttonPin;
-		btnUpdateDelegate buttonUseDelegate;
-//		cb_use cb_buttonUse;
+	boolean buttonVal = HIGH;   // value read from button
+	boolean buttonLast = HIGH; // buffered value of the button's previous state
+	boolean DCwaiting = false; // whether we're waiting for a double click (down)
+	boolean DConUp = false; // whether to register a double click on next release, or whether to wait and click
+	boolean singleOK = true;    // whether it's OK to do a single click
+	long downTime = -1;         // time the button was pressed down
+	long upTime = -1;           // time the button was released
+	long lastPressAndHoldTime = -1; // time the pressAndHold was activated last
+	long pressAndHoldRepetGap = 100;
 
-		Timer buttonTimer;
-
-	public:
-		ButtonActions(int buttonPin, btnUpdateDelegate buttonUseDelegate)
-		{
-			m_buttonPin = buttonPin;
+	boolean ignoreUp = false; // whether to ignore the button release because the click+hold was triggered
+	boolean waitForUp = false; // when held, whether to wait for the up event
+	boolean holdEventPast = false; // whether or not the hold event happened already
+	boolean longHoldEventPast = false; // whether or not the long hold event happened already
+	int m_buttonPin;
+	bool enablePressAndHold = true; //This will fire click events on button hold (so will not fire BTN_LONG_CLICK and BTN_HOLD_CLICK)
+public:
+	ButtonActions(int buttonPin, btnUpdateDelegate buttonUseDelegate)
+	{
+		m_buttonPin = buttonPin;
 //			cb_buttonUse = buttonUse;
-			this->buttonUseDelegate = buttonUseDelegate;
-			buttonTimer.initializeMs(80, TimerDelegate(&ButtonActions::actOnButton, this)).start();
-		};
+		this->buttonUseDelegate = buttonUseDelegate;
+		buttonTimer.initializeMs(80, TimerDelegate(&ButtonActions::actOnButton, this)).start();
+	};
 
-		int checkButton()
+	int checkButton()
+	{
+		int event = 0;
+		buttonVal = digitalRead(m_buttonPin);
+		// Button pressed down
+		if (buttonVal == LOW && buttonLast == HIGH
+				&& (millis() - upTime) > debounce)
 		{
-			int event = 0;
-			buttonVal = digitalRead(m_buttonPin);
-			// Button pressed down
-			if (buttonVal == LOW && buttonLast == HIGH
-					&& (millis() - upTime) > debounce)
+			downTime = millis();
+			ignoreUp = false;
+			waitForUp = false;
+			singleOK = true;
+			holdEventPast = false;
+			longHoldEventPast = false;
+			if ((millis() - upTime)
+					< DCgap&& DConUp == false && DCwaiting == true)
+				DConUp = true;
+			else
+				DConUp = false;
+			DCwaiting = false;
+		}
+		// Button released
+		else if (buttonVal == HIGH && buttonLast == LOW
+				&& (millis() - downTime) > debounce)
+		{
+			if (not ignoreUp)
 			{
-				downTime = millis();
-				ignoreUp = false;
-				waitForUp = false;
-				singleOK = true;
-				holdEventPast = false;
-				longHoldEventPast = false;
-				if ((millis() - upTime)
-						< DCgap&& DConUp == false && DCwaiting == true)
-					DConUp = true;
+				upTime = millis();
+				if (DConUp == false)
+					DCwaiting = true;
 				else
-					DConUp = false;
-				DCwaiting = false;
-			}
-			// Button released
-			else if (buttonVal == HIGH && buttonLast == LOW
-					&& (millis() - downTime) > debounce)
-			{
-				if (not ignoreUp)
 				{
-					upTime = millis();
-					if (DConUp == false)
-						DCwaiting = true;
-					else
-					{
-						event = 2;
-						DConUp = false;
-						DCwaiting = false;
-						singleOK = false;
-					}
+					event = 2;
+					DConUp = false;
+					DCwaiting = false;
+					singleOK = false;
 				}
 			}
-			// Test for normal click event: DCgap expired
-			if (buttonVal == HIGH && (millis() - upTime) >= DCgap
-					&& DCwaiting == true && DConUp == false && singleOK == true
-					&& event != 2)
+		}
+		// Test for normal click event: DCgap expired
+		if (buttonVal == HIGH && (millis() - upTime) >= DCgap
+				&& DCwaiting == true && DConUp == false && singleOK == true
+				&& event != 2)
+		{
+			event = 1;
+			DCwaiting = false;
+		}
+
+		if (enablePressAndHold) {
+			// Test for hold
+			long current = millis();
+			if (buttonVal == LOW && ((current - downTime) >= holdTime) && (current - pressAndHoldRepetGap)>= lastPressAndHoldTime)
 			{
-				event = 1;
-				DCwaiting = false;
+				lastPressAndHoldTime = current;
+				buttonUseDelegate(ButtonAction::BTN_CLICK);
 			}
+		}
+		else {
 			// Test for hold
 			if (buttonVal == LOW && (millis() - downTime) >= holdTime)
 			{
@@ -144,31 +154,37 @@ class ButtonActions
 					}
 				}
 			}
-			buttonLast = buttonVal;
-			return event;
 		}
 
-		void actOnButton()
-		{
-			// Get button event and act accordingly
-			int b = checkButton();
-			ButtonAction act;
-			if (b == 1)
-				act = BTN_CLICK;
-			if (b == 2)
-				act = BTN_DOUBLE_CLICK;
-			if (b == 3)
-				act = BTN_LONG_CLICK;
-			if (b == 4)
-				act = BTN_HOLD_CLICK;
+		buttonLast = buttonVal;
+		return event;
+	}
 
-			if ((b==1 || b==2 || b==3 || b==4)) {
+	void actOnButton()
+	{
+		// Get button event and act accordingly
+		int b = checkButton();
+		ButtonAction act;
+		if (b == 1)
+			act = BTN_CLICK;
+		if (b == 2)
+			act = BTN_DOUBLE_CLICK;
+		if (b == 3)
+			act = BTN_LONG_CLICK;
+		if (b == 4)
+			act = BTN_HOLD_CLICK;
+
+		if ((b==1 || b==2 || b==3 || b==4)) {
 //				ButtonUseEvent bue = { act };
 
-				buttonUseDelegate(act);
+			buttonUseDelegate(act);
 //				cb_buttonUse(bue);
-			}
 		}
-	};
+	}
+private:
+	btnUpdateDelegate buttonUseDelegate;
+	Timer buttonTimer;
+	TimerDelegate holdTimerDelegate;
+};
 
 #endif /* INCLUDE_ButtonActions_ */
